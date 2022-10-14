fcc194d3f329ceba4dd27a95dcdec1013fcd1ce8
aron-helu <aronmilahela@gmail.com>
2022-10-14 19:25:37 +0300

    fix linters

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,43 +1,43 @@
 require_relative '../solver'
 
 describe Solver do
-    before (:each) do
-        @solver = Solver.new
+  before(:each) do
+    @solver = Solver.new
+  end
+
+  context 'factorial method' do
+    it 'take N and return factorial for that number' do
+      result = @solver.factorial(0)
+      expect(result).to eq 1
     end
-    
-    context 'factorial method' do
-        it 'take N and return factorial for that number' do
-            result = @solver.factorial(0)
-            expect(result).to eq 1
-        end
-    end    
-    
-    context 'reverse method' do
-        it 'take word and return the reverse for that word' do
-            result = @solver.reverse('hello')
-            expect(result).to eq 'olleh'
-        end
+  end
+
+  context 'reverse method' do
+    it 'take word and return the reverse for that word' do
+      result = @solver.reverse('hello')
+      expect(result).to eq 'olleh'
+    end
+  end
+
+  context 'fizzbuzz method' do
+    it 'when n is divisible by 3' do
+      result = @solver.fizzbuzz(12)
+      expect(result).to eq 'fizz'
     end
 
-    context 'fizzbuzz method' do
-        it 'when n is divisible by 3' do
-            result = @solver.fizzbuzz(12)
-            expect(result).to eq 'fizz'
-        end
-
-        it 'when n is divisible by 5' do
-            result = @solver.fizzbuzz(20)
-            expect(result).to eq 'buzz'
-        end
-
-        it 'when n is divisible by 15' do
-            result = @solver.fizzbuzz(45)
-            expect(result).to eq 'fizzbuzz'
-        end
-
-        it 'when n is not fizzbuzz return n' do
-            result = @solver.fizzbuzz(7)
-            expect(result).to eq 7
-        end
+    it 'when n is divisible by 5' do
+      result = @solver.fizzbuzz(20)
+      expect(result).to eq 'buzz'
     end
+
+    it 'when n is divisible by 15' do
+      result = @solver.fizzbuzz(45)
+      expect(result).to eq 'fizzbuzz'
+    end
+
+    it 'when n is not fizzbuzz return n' do
+      result = @solver.fizzbuzz(7)
+      expect(result).to eq 7
+    end
+  end
 end
